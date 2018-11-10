86c92fc78d1a12a3b4b231085c8bcfad6260978d
Renato Gama <renatoargh@gmail.com>
2018-11-10 15:28:22 +0100

    Removes bluebird, using safe routes

--- server/index.js
+++ server/index.js
@@ -15,59 +15,59 @@ app.use(express.static(STATIC_PATH))
 app.use(methodOverride('_method'))
 app.use(bodyParser.json({ extend: true }))
 
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   res.locals.path = req.path
-  next()
+  await next()
 })
+
+const safe = (middleware) => (req, res, next) => Promise.resolve(middleware(req, res, next)).catch(next)
 
 async function createAggregator (aggregator) {
   const topicArn = await sns.createTopic(aggregator.name)
 
   Object.assign(aggregator, { topicArn })
   await dynamodb.createAggregator(aggregator, LAMBDA_ARN)
-
   await sns.subscribeTo(aggregator.events, LAMBDA_ARN, topicArn)
 }
 
-app.post('/events', async (req, res) => {
+app.post('/events', safe(async (req, res) => {
   const { topicArn, message } = req.body
   await sns.publish(topicArn, message)
 
   res.status(204).json({})
-})
+}))
 
-app.post('/aggregators', async (req, res) => {
+app.post('/aggregators', safe(async (req, res) => {
   await createAggregator(req.body)
   res.status(204).json({})
-})
+}))
 
-app.get('/aggregators', async (req, res) => {
+app.get('/aggregators', safe(async (req, res) => {
   const aggregators = await dynamodb.listAggregators()
   res.render('aggregators', { aggregators })
-})
+}))
 
-app.get('/subscriptions', async (req, res) => {
+app.get('/subscriptions', safe(async (req, res) => {
   const subscriptions = await dynamodb.listSubscriptions()
   res.render('subscriptions', { subscriptions })
-})
+}))
 
-app.get('/', (req, res) => {
+app.get('/', safe((req, res) => {
   res.redirect('/aggregators')
-})
+}))
 
 app.listen(PORT, () => {
   console.log('events-aggregator ui listening on port', PORT)
 })
 
-// async function main() {
-//   await createAggregator({
-//     name: 'OrderRequirementsFulfilled',
-//     groupBy: 'orderId',
-//     events: [
-//       'arn:aws:sns:eu-central-1:423715701352:CreditReservation',
-//       'arn:aws:sns:eu-central-1:423715701352:InventoryReservation'
-//     ]
-//   }, LAMBDA_ARN)
-// }
+async function main() {
+  await createAggregator({
+    name: 'AnotherEvent',
+    groupBy: 'orderId',
+    events: [
+      'arn:aws:sns:eu-central-1:423715701352:CreditReservation'
+    ]
+  }, LAMBDA_ARN)
+}
 
-// main()
+main()
